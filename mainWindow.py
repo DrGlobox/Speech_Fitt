4a6d5365a1ffb4ef8ef43bcf01f7d9387f4051ab
DrGlobox <locap25@gmail.com>
2013-05-14 19:20:54 +0200

    Browser

--- mainWindow.py
+++ mainWindow.py
@@ -3,6 +3,8 @@ from PyQt4.QtGui import *
 from PyQt4.QtCore import *
 import sys
 
+from browser import *
+
 class MainWindow():
 
     def __init__(self):
@@ -22,16 +24,20 @@ class MainWindow():
         
         self.cmdButton = QPushButton("Lancer écoute")
 
+        self.browser = BrowserWidget(self.window)
+
         layout = QHBoxLayout()
         layout.addWidget(self.prevButton)
         layout.addWidget(self.nextButton)
         layout.addStretch()
         layout.addWidget(self.cmdButton)
 
-
+        bigLayout = QVBoxLayout()
+        bigLayout.addLayout(layout)
+        bigLayout.addWidget(self.browser)
 
         self.menu = QWidget()
-        self.menu.setLayout(layout)
+        self.menu.setLayout(bigLayout)
         self.window.setCentralWidget(self.menu)
 
     
